391ef1d78aae70c29006712ea9c7a1aa3b36dc48
yu hyun ju <a01085249773@gmail.com>
2024-05-28 17:30:10 +0900

    Data Delivery -> (자식 > 부모  > 자식: prop, event방식)

--- src/views/Ch05DataDelivery/Exam02EventEmit/Child.vue
+++ src/views/Ch05DataDelivery/Exam02EventEmit/Child.vue
@@ -4,24 +4,52 @@
       <div class="card-body">
         <div>
             <h6>[자식 -> 부모]</h6>
-            <button class="btn btn-secondary btn-sm me-2" 
-                    @click="$emit('child-event-1', 'value1')">
+            <button class="btn btn-dark btn-sm me-2" 
+                    @click="$emit('child-event-1', data1)">
                             child-event-1 발생</button>
-            <button class="btn btn-secondary btn-sm" 
+            <button class="btn btn-dark btn-sm" 
                     @click="handleChildEvent2">
                             child-event-2 발생</button>
+        </div>
+
+        <div class="mt-3">
+            <h6>[자식 -> 부모 -> 자식]</h6>
+            <button class="btn btn-warning btn-sm me-2"
+                    @click="handleIncrementEvent">증가(increment-event 이벤트 발생)</button>
+            <button class="btn btn-warning btn-sm"
+                    @click="handleDecrementEvent">감소(decrement-event 이벤트 발생)</button>
+            
+            <p class="mt-2">Counter: {{ prop.counter }}</p>
         </div>
       </div>
   </div>
 </template>
 
 <script setup>
+import { ref, defineProps } from 'vue';
 
-const emit = defineEmits(['child-event-1', 'child-event-2']);
+let data1 = ref("value1");
+let data2 = ref('value2');
+
+const emit = defineEmits(['child-event-1', 'child-event-2',
+                          'increment-event', 'decrement-event']);
 
 function handleChildEvent2() {
-    emit('child-event-2', 'value1', 'value2');
+    emit('child-event-2', data1.value, data2.value);
 }
+
+function handleIncrementEvent() {
+    // 자식 -> 부모
+    emit('increment-event', 1);
+}
+
+function handleDecrementEvent() {
+    // 자식 -> 부모
+    emit('decrement-event', 1);
+}
+
+// fallthrough  가 되지 않게 defineprops 설정
+const prop = defineProps(["counter"]);
 
 </script>
 
--- src/views/Ch05DataDelivery/Exam02EventEmit/index.vue
+++ src/views/Ch05DataDelivery/Exam02EventEmit/index.vue
@@ -3,12 +3,16 @@
     <div class="card-header">Exam02EventEmit</div>
       <div class="card-body">
         <Child @child-event-1="handleChildEvent1"
-               @child-event-2="handleChildEvent2"/>
+               @child-event-2="handleChildEvent2"
+               @increment-event="handleIncrementEvent"
+               @decrement-event="handleDecrementEvent"
+               :counter="counter"/>
       </div>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import Child from './Child.vue';
 
 function handleChildEvent1(data1) {
@@ -20,6 +24,18 @@ function handleChildEvent2(data1, data2) {
     console.log("handleChildEvent2() 실행");
     console.log("data1: ", data1);
     console.log("data2: ", data2);
+}
+
+// 상태 정의
+const counter = ref(0);
+
+// 상태 값 변경
+function handleIncrementEvent(amount) {
+    counter.value += amount;
+}
+
+function handleDecrementEvent(amount) {
+    counter.value -= amount;
 }
 
 </script>
